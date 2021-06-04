977e42d73a4c9457e86b5ca046e5f4d2ef4a6399
Mnkith <munkithabid@gmail.com>
2021-06-04 10:37:33 -0500

    create game controller

---
+++ chessmate-fe/src/piece.js
@@ -0,0 +1,9 @@
+class Piece {
+  constructor (name, symbol, color, position, initial){
+    this.name = name
+    this.symbol = symbol 
+    this.color = color
+    this.position = position
+    this.initial = initial
+  }
+}

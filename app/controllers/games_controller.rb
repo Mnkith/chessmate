214225baeecccf2b9afae33e057b8a946187ed01
Mnkith <munkithabid@gmail.com>
2021-06-04 14:41:20 -0500

    create game class file

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,9 @@ class GamesController < ApplicationController
     game = Game.find_by_id params[:id]
     render json: game, include: [:pieces]
   end
+
+  def index
+    games = Game.all
+    render json: games
+  end
 end
--- chessmate-fe/src/game.js
+++ chessmate-fe/src/game.js
@@ -1,3 +1,44 @@
-class Game{
-  constructor()
+class Game {
+  constructor(event, site, white = 'White', black = 'Black', pieces = []) {
+    this.event = event
+    this.site = site
+    this.white = white
+    this.black = black
+    this.piecs = pieces
+  }
+
+  static fetchAllGames() {
+    fetch(`http://127.0.0.1:3000/games`)
+      .then(resp => resp.json())
+    // .then(json => initializeBoard(json));
+  }
+
+  static fetchGame(round = 1) {
+    let g = {}
+    return fetch(`http://127.0.0.1:3000/games/${round}`)
+      .then(resp => resp.json())
+      .then(json => json);
+    // return 'gggg'
+    // return g
+  }
+
+  setDefaultLayout() {
+    this.pieces = Game.fetchGame().pieces
+  }
+  static initializeBoard(game) {
+    pieces = game.pieces
+    pieces.forEach(p => {
+      div = document.createElement('div')
+      div.className = 'piece'
+      div.innerHTML = p.symbol
+      div.style.gridArea = p.position
+      board = document.getElementById('board')
+      board.appendChild(div)
+    })
+  }
+
+
+  // set persist(){
+
+  // }
 }

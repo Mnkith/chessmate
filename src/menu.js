4c9b3d26fbcbf773bc2517da1ce62a9558f22d37
Mnkith <munkithabid@gmail.com>
2021-06-10 12:39:05 -0500

    add show method to Menu class

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,12 @@
 class Menu {
   static _menus = ['main-menu', 'new-game-menu', 'in-game-menu', 'three-boards']
 
-  
+  static show(menu){
+    Menu._menus.forEach( (men) => {
+      if(men !== menu)
+        document.getElementById(men).style.visibility = 'hidden'
+    })
+    document.getElementById(menu).style.visibility = 'visible'
+  }
+
 }

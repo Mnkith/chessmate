266fd8875ac829ffa3b3daf5782ffed369140325
Mnkith <munkithabid@gmail.com>
2021-06-10 23:37:38 -0500

    create nested resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # get 'pieces/index', to: 'pieces#index'
   get '/pieces', to: 'pieces#index'
   get 'games', to: 'games#index'
-
+  resources :games do
+    resources :pieces
+  end
   post 'games', to: 'games#create'
 end
